8a8b82d9a49a591ebb180728006a7137823dece2
richy <richy_riched@yahoo.com>
2019-06-27 21:37:16 -0700

    Updated GUI

--- src/ARK/Illuminati/gui/GUI.java
+++ src/ARK/Illuminati/gui/GUI.java
@@ -5,23 +5,15 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
 
-    /*
+
     public static Clip audioClip ;
     private JPanel panel2;
-    private SpellsPanel spellAreap1;
-    private MonstersPanel monsterareap1 ;
-    private MonstersPanel monsterAreap2 ;
-    private SpellsPanel spellAreap2 ;
-    public JLabel deckp1 ;
-    public JLabel deckp2 ;
-    public JLabel lifep1 ;
-    public JLabel lifep2 ;
-    public JButton gravep1 ;
-    public JButton gravep2 ;
-    private NextPhBut nextPhase ;
+    public JLabel deck ;
+    public JButton grave ;
+    private NextActionButton nextAction ;
     private JLabel p1name ;
     private JLabel p2name ;
-    public JLabel currphase ;
+    public JLabel curraction ;
     private EndTurnButton endTurn ;
     private JPanel panel1 ;
     private HandPanel handp1 ;
@@ -42,6 +34,8 @@ public class GUI extends JFrame {
     public JScrollPane getSp1() { return sp1 ; }
 
     public void setSp1(JScrollPane sp1) { this.sp1 = sp1 ;  }
+
+    public JScrollPane getSp2() { return sp2 ; }
 
     public void setSp2( JScrollPane sp2) { this.sp2 = sp2 ; }
 
@@ -65,98 +59,33 @@ public class GUI extends JFrame {
 
     public void setPanel2( JPanel panel2 ) { this.panel2 = panel2 ; }
 
+    public JLabel getDeck(){ return deck ; }
 
-    public SpellsPanel getSpellAreap1() {
-        return spellAreap1;
+    public void setDeck( JLabel deck ) { this.deck = deck ; }
+
+    public JButton getGrave() {
+        return grave;
     }
 
-    public void setSpellAreap1(SpellsPanel spellAreap1) {
-        this.spellAreap1 = spellAreap1;
+    public void setGrave(JButton gravep1) {
+        this.grave = grave;
     }
 
-    public MonstersPanel getMonsterareap1() {
-        return monsterareap1;
-    }
+    public NextActionButton getNextAction(){ return nextAction ; }
 
-    public void setMonsterareap1(MonstersPanel monsterareap1) {
-        this.monsterareap1 = monsterareap1;
-    }
-
-    public MonstersPanel getMonsterAreap2() {
-        return monsterAreap2;
-    }
-
-    public void setMonsterAreap2(MonstersPanel monsterAreap2) {
-        this.monsterAreap2 = monsterAreap2;
-    }
-
-    public SpellsPanel getSpellAreap2() { return spellAreap2; }
-
-    public void setSpellAreap2(SpellsPanel spellAreap2) { this.spellAreap2 = spellAreap2; }
-
-    public JLabel getDeckp1(){ return deckp1 ; }
-
-    public void setDeckp1(JLabel deckp1 ) { this.deckp1 = deckp1 ; }
-
-    public JLabel getDeckp2(){ return deckp2 ; }
-
-    public void setDeckp2(JLabel deckp2 ){ this.deckp2 = deckp2 ; }
-
-    public JLabel getLifep1() {
-        return lifep1;
-    }
-
-    public void setLifep1(JLabel lifep1) {
-        this.lifep1 = lifep1;
-    }
-
-    public JLabel getLifep2() {
-        return lifep2;
-    }
-
-    public void setLifep2(JLabel lifep2) {
-        this.lifep2 = lifep2;
-    }
-
-    public JButton getGravep1() {
-        return gravep1;
-    }
-
-    public void setGravep1(JButton gravep1) {
-        this.gravep1 = gravep1;
-    }
-
-    public JButton getGravep2() {
-        return gravep2;
-    }
-
-    public void setGravep2(JButton gravep2) {
-        this.gravep2 = gravep2;
-    }
-
-    public NextPhBut getNextPhase(){ return nextPhase ; }
-
-    public void setNextPhase( NextPhBut nextPhase ){ this.nextPhase = nextPhase ; }
+    public void setNextActionButton( NextActionButton nextAction ){ this.nextAction = nextAction ; }
 
     public JLabel getP1name( ) { return p1name ; }
 
     public void setP1name( JLabel p1name ){ this.p1name = p1name ; }
 
-    public JLabel getP2name() {
-        return p2name;
-    }
+    public JLabel getP2name() { return p2name; }
 
-    public void setP2name(JLabel p2name) {
-        this.p2name = p2name;
-    }
+    public void setP2name(JLabel p2name) { this.p2name = p2name; }
 
-    public JLabel getCurrphase() {
-        return currphase;
-    }
+    public JLabel getCurraction() { return curraction; }
 
-    public void setCurrphase(JLabel currphase) {
-        this.currphase = currphase;
-    }
+    public void setCurraction(JLabel curraction) { this.curraction = curraction; }
 
     public EndTurnButton getEndTurn() { return endTurn ; }
 
@@ -166,21 +95,9 @@ public class GUI extends JFrame {
 
     public void setPanel( JPanel panel1 ){ this.panel1 = panel1 ; }
 
-    public HandPanel getHandp1(){ return handp1 ; }
-
-    public void setHandp1() { this.handp1 = handp1 ; }
-
-    public HandPanel getHandp2() {
-        return handp2;
-    }
-
-    public void setHandp2(HandPanel handp2) {
-        this.handp2 = handp2;
-    }
-
     public JScrollPane getPan(){ return pan ; }
 
     public void setPan( JScrollPane pan ){ this.pan = pan ; }
-    */
+
 
 }
