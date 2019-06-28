b968511f80e0aeeeabc8f1e9f10fbe6a9a36c835
richy <richy_riched@yahoo.com>
2019-06-27 18:04:30 -0700

    Creating Packages needed

---
+++ src/ARK/Illuminati/board/Board.java
@@ -0,0 +1,4 @@
+package ARK.Illuminati.board;
+
+public class Board {
+}
---
+++ src/ARK/Illuminati/board/player/Deck.java
@@ -0,0 +1,10 @@
+package ARK.Illuminati.board.player;
+
+public class Deck {
+
+    //instance variables
+    private static ArrayList<Card> illuminati ;
+    private static ArrayList<Card> group ;
+    private static ArrayList<Card> special ;
+
+}
