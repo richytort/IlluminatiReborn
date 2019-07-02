1466c4b54399dc606149ddbe9b2ff2a3b8aeb593
aschein80 <alexschein12345@gmail.com>
2019-07-02 10:51:33 -0700

    javadoc updates

--- src/ARK/Illuminati/gui/NextActionButton.java
+++ src/ARK/Illuminati/gui/NextActionButton.java
@@ -3,8 +3,15 @@ package ARK.Illuminati.gui;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class deals with the next action button
+ */
 public class NextActionButton extends JButton {
 
+    /**
+     * Next action button method
+     * @param string specified string for the next action button
+     */
     public NextActionButton(String string ){
         super(string);
         this.setPreferredSize(new Dimension(214, 53)) ;
